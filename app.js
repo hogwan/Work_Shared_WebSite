1816dfa4c45d6c4edefa861fbb97e2bb04ef65b7
Hogwan <asdf36597@naver.com>
2024-11-19 14:35:57 +0900

    모든 페이지에 대한 get 만들어놓음

--- app.js
+++ app.js
@@ -11,8 +11,47 @@ app.get('/currenttime', function(req, res) {
     res.send('<h1>' + new Date().toISOString() + '</h1>');
 } );
 
-app.get('/', function(req, res){
+app.get('/board', function(req, res){
     res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
+});
+
+app.get('/image-upload', function(req, res){
+
+});
+
+app.get('/login', function(req,res){
+
+});
+
+app.get('/my-info', function(req,res){
+
+});
+
+app.get('/search', function(req,res){
+
+});
+
+app.get('/find-password', function(req,res)
+{
+
+});
+
+
+app.get('/users', function(req, res) {
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    
+    let responseData = '<ul>';
+    
+    for(const user of existingUsers) {
+        responseData += '<li>' + user +'</li>';
+    }
+    
+    responseData += '</ul>';
+    
+    res.send(responseData);
 });
 
 app.post('/store-user', function(req, res){
@@ -28,23 +67,6 @@ app.post('/store-user', function(req, res){
     fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
     res.send('<h1>Username stored!</h1>');
-});
-
-app.get('/users', function(req, res) {
-    const filePath = path.join(__dirname, 'data', 'users.json');
-    
-    const fileData = fs.readFileSync(filePath);
-    const existingUsers = JSON.parse(fileData);
-
-    let responseData = '<ul>';
-
-    for(const user of existingUsers) {
-        responseData += '<li>' + user +'</li>';
-    }
-
-    responseData += '</ul>';
-
-    res.send(responseData);
 });
 
 app.listen(3000);
